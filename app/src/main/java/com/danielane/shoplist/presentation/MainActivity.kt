b4dd9469d1a0b77c42c936040bd549a58ede8118
DanielLane228 <warfacealpine10@gmail.com>
2021-12-03 23:20:24 +0800

    Project is created

---
+++ app/src/main/java/com/danielane/shoplist/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.danielane.shoplist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.danielane.shoplist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
